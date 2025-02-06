e88bf60f51122e139bddc2d9e405d2599a07daa6
Marvindlls <marvindillenseger@gmail.com>
2025-02-06 13:55:22 +0100

    add edit route for doctor profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Info from "./pages/Info/Info";
+import Edit from "./pages/Edit/Edit";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/info/:doctorId" element={<Info />} />
+        <Route path="/edit/:doctorId" element={<Edit />} />
       </Routes>
     </BrowserRouter>
   )

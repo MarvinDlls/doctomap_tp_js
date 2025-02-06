647fac282ed6d2b0473900a747c040eed2906cf2
Marvindlls <marvindillenseger@gmail.com>
2025-02-06 15:17:21 +0100

    add Create page route to App component for new doctor entry

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Info from "./pages/Info/Info";
 import Edit from "./pages/Edit/Edit";
+import Create from "./pages/Create/Create";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/info/:doctorId" element={<Info />} />
         <Route path="/edit/:doctorId" element={<Edit />} />
+        <Route path="/create" element={<Create />}/>
       </Routes>
     </BrowserRouter>
   )
